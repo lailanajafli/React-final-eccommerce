8b3fadea5bc2785fad473a33dd823993defa92f3
lailanajafli <laila.nac2001@gmail.com>
2024-03-27 03:25:44 +0400

    added a Link instead of a button for ViewCart.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,69 +1,11 @@
-//Detail.jsx
-
-
-// import React from 'react';
-
-// import Card from 'react-bootstrap/Card';
-// import './Detail.css';
-// import Navbar2 from '../navbar/Navbar2';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { addToCard, removeFromCard } from '../../app/features/products/productSlice';
-
-// export default function Shopping() {
-//     const dispatch = useDispatch();
-//     const cards = useSelector(state => state.product.cartItems);
-
-//     const handleCard = (id) => {
-//         dispatch(addToCard(id));
-//     }
-
-//     const removeCard = (id) => {
-//         dispatch(removeFromCard(id));
-//     }
-
-//     const checkIfProductInStore = (id) => Boolean(cards?.find(el => el.id === id));
-
-//     return (
-//         <div>
-//             <Navbar2 />
-//             {cards.map(el => (
-//                 <div key={el.id}>
-//                     <div className="d-card">
-//                         <img className="d-card-image" src={el.image} alt={el.title} />
-//                         <div>
-//                             <p className="d-card-title">{el.title}</p>
-//                             <p className="d-card-description">{el.description}</p>
-//                             <div className="d-button-container">
-//                                 <div className="d-price">{el.price} $</div>
-//                             </div>
-//                             {checkIfProductInStore(el.id) &&
-//                                 <button onClick={() => removeCard(el.id)} variant="danger" className="d-remove-button">X</button>
-//                             }
-//                         </div>
-//                     </div>
-//                 </div>
-//             ))}
-//             <div>
-//                 <p>Total Price: {cards.reduce((a, b) => a + b.price, 0)}$</p>
-//                 <button>Proceed to Checkout</button>
-//             </div>
-//         </div>
-//     );
-// }
-
-
-
-
-
-
 
 // Sidebar.jsx
-/* Sidebar.jsx */
-// Sidebar.jsx
+
+import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCard, removeFromCard } from '../../app/features/products/productSlice';
-import './Sidebar.css'; // Sidebar stilleri
+import './Sidebar.css'; 
 
 export default function Sidebar({ isOpen, toggleSidebar }) {
     const dispatch = useDispatch();
@@ -108,10 +50,10 @@ export default function Sidebar({ isOpen, toggleSidebar }) {
                                 </div>
                             ))}
                         </div>
+                    </div>
                     <div className="checkout">
                         <p>Total Price: {cards.reduce((a, b) => a + b.price, 0)}$</p>
-                        <button className='add-to-cart-button'>View Cart</button>
-                    </div>
+                        <Link to="/viewcart"><button className="add-to-cart-button">View Cart</button></Link>
                     </div>
                 </div>
             </div>
