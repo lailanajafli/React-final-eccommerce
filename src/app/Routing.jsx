cf9d03b217b91804827c32f88091b3adaea1e4e6
lailanajafli <laila.nac2001@gmail.com>
2024-03-29 05:35:36 +0400

    for Route

---
+++ src/app/Routing.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import WomenProducts from '../components/WomenProducts'
+import Accessories from '../components/Accessories'
+import MenProducts from '../components/MenProducts'
+import Electronics from '../components/Electronics'
+import Products from '../components/Products'
+import ViewCart from '../components/viewcart/ViewCart'
+import Pay from '../components/pay/Pay'
+import { Route, Routes } from 'react-router'
+
+const Routing = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/women" element={<WomenProducts />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/men" element={<MenProducts />} />
+        <Route path="/electronics" element={<Electronics />} />
+        <Route path="/viewcart" element={<ViewCart />} />
+        <Route path="/pay" element={<Pay />} />
+      </Routes>
+  )
+}
+
+export default Routing
