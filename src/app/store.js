ade46d258608a7573a613fef42c969ea17b8bc0e
lailanajafli <laila.nac2001@gmail.com>
2024-03-29 05:35:00 +0400

    added filteredValue

--- src/app/store.js
+++ src/app/store.js
@@ -3,9 +3,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import counterReducer from './features/counter/counterSlice.js'
 import productReducer from './features/products/productSlice.js'
+import filteredValue from "./features/filteredValue/index.js"
 export default configureStore({
     reducer: {
         counter: counterReducer,
         product: productReducer,
+        filteredValue:filteredValue
     }
 })
