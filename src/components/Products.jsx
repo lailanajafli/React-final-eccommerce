92adfd7284aadeaa05aef94a87866159c7888cc2
lailanajafli <laila.nac2001@gmail.com>
2024-03-31 07:23:33 +0400

    home page added 3 big image

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -17,7 +17,7 @@ export default function Products() {
   }, [allProducts]);
 
   useEffect(() => {
-    let sortedData = [...data]; 
+    let sortedData = [...data];
     if (sortBy === "asc") {
       sortedData.sort((a, b) => a.price - b.price);
     } else if (sortBy === "desc") {
@@ -36,8 +36,40 @@ export default function Products() {
 
   return (
     <div>
-      <Navbar />
+      <Navbar /> 
       <div className="products-header"></div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem', justifyContent: 'center' }}>
+        <div className="big-img-card">
+          <img className="img-card" src="../braclet.jpg" alt="" />
+          <div className="big-card-body">
+            <h1 className="card-tit">John Hardy</h1>
+            <p className="card-sub-tit">Silver Dragon Station Chain Bracelet</p>
+            <p className="card-infor">From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.</p>
+            <button className="big-card-img-btn"> See More</button>
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', flexWrap: 'wrap' }}>
+          <div className="big-img-card" style={{ width: '492px' }}>
+            <img className="img-card" style={{ width: '100%' }} src="../t-shirt.jpg" alt="" />
+            <div className="big-card-body">
+            <h1 className="card-tit">DANVOUY</h1>
+            <p className="card-sub-tit">Womens T Shirt Casual Cotton Short</p>
+            <p className="card-infor">95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.</p>
+            <button className="big-card-img-btn"> See More</button>
+          </div>
+          </div>
+          <div className="big-img-card" style={{ width: '492px' }}>
+            <img className="img-card" style={{ width: '100%' }} src="../bags.jpg" alt="" />
+            <div className="big-card-body">
+            <h1 className="card-tit">Fjallraven</h1>
+            <p className="card-sub-tit">Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+            <p className="card-infor">Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+            <button className="big-card-img-btn"> See More</button>
+          </div>
+          </div>
+        </div>
+      </div>
+
       <div className="products-container">
         {data?.map((el) => (
           <MCard key={el.id} {...el} />
